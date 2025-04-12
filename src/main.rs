050e934862f05cbb1b1072ea0a6049e0a18869a4
nervana21 <205626986+nervana21@users.noreply.github.com>
2025-04-12 02:33:50 -0400

    refactor(generator): add initial project structure with configuration files, API definitions, and basic functionality
    - added basic testing functionality
    - Implemented basic functionality in src/lib.rs and main.rs for the project.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use bitcoin_rpc_codegen::generator::main as generator_main;
+
+fn main() -> Result<()> {
+    generator_main()?;
+    Ok(())
 }
