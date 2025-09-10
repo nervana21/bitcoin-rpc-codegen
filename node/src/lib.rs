a991a5d92426067a0a34151c1083d0a0e24682d0
nervana21 <205626986+nervana21@users.noreply.github.com>
2025-09-09 22:54:01 -0400

    Remove unused abstractions
    There is no current implementation of dynamic runtime version detection.
    So, this abstraction is removed.
    
    Futher, removed unit tests that have become obsolete and/or redundant.

--- node/src/lib.rs
+++ node/src/lib.rs
@@ -13,7 +13,6 @@ use tokio::sync::{Mutex, RwLock};
 use tracing::{debug, error, info};
 pub mod test_config;
 pub use config::{BitcoinConfig, Config};
-use types::Version;
 use std::process::Stdio;
 pub use test_config::TestConfig;
 
@@ -21,14 +20,12 @@ pub use test_config::TestConfig;
 #[derive(Debug, Clone)]
 pub struct NodeState {
     pub is_running: bool,
-    pub version: Version,
 }
 
 impl Default for NodeState {
     fn default() -> Self {
         Self {
             is_running: false,
-            version: Version::from_string("v29").unwrap(),
         }
     }
 }
