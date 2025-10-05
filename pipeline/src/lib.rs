5fa492fc67b562f30878b23aec7d601bdaf612f7
nervana21 <205626986+nervana21@users.noreply.github.com>
2025-10-04 21:14:37 -0400

    Bump midas crate types version to v1.0.0

--- pipeline/src/lib.rs
+++ pipeline/src/lib.rs
@@ -360,7 +360,7 @@ documentation = "https://docs.rs/bitcoin-rpc-midas"
 anyhow = "1.0"
 async-trait = "0.1"
 bitcoin = {{ version = "0.32.6", features = ["rand", "serde"] }}
-bitcoin-rpc-types = "0.1.2"
+bitcoin-rpc-types = "1.0.0"
 reqwest = {{ version = "0.12.15", default-features = false, features = [
     "json",
     "rustls-tls",
